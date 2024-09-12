d9499c819dd75570d140170b81648fd0def67fd0
Nikita Semchenkov <72917811+smcnikita@users.noreply.github.com>
2024-09-12 13:54:01 +0300

    feature: add focus trap in popper

--- src/components/BasePopper.vue
+++ src/components/BasePopper.vue
@@ -1,35 +1,39 @@
 <template>
   <Transition>
-    <div v-if="isShowPopper" class="popper">
-      <button
-        class="popper__button"
-        @click.prevent="$emit('changeGrid', 3)"
-        @mouseenter="onMouseenter(3)"
-        @mouseleave="onMouseleave"
-      >
-        3 color grid
-      </button>
-      <button
-        class="popper__button"
-        @click.prevent="$emit('changeGrid', 5)"
-        @mouseenter="onMouseenter(5)"
-        @mouseleave="onMouseleave"
-      >
-        5 color grid
-      </button>
-      <button class="popper__button" @click.prevent="$emit('newColors')">new colors</button>
-      <button class="popper__button" @click.prevent="$emit('clear')">clear</button>
-    </div>
+    <template v-if="isShowPopper">
+      <focus-trap :active="isShowPopper">
+        <div class="popper">
+          <button
+            class="popper__button"
+            @click.prevent="$emit('changeGrid', 3)"
+            @mouseenter="onMouseenter(3)"
+            @mouseleave="onMouseleave"
+          >
+            3 color grid
+          </button>
+          <button
+            class="popper__button"
+            @click.prevent="$emit('changeGrid', 5)"
+            @mouseenter="onMouseenter(5)"
+            @mouseleave="onMouseleave"
+          >
+            5 color grid
+          </button>
+          <button class="popper__button" @click.prevent="$emit('newColors')">new colors</button>
+          <button class="popper__button" @click.prevent="$emit('clear')">clear</button>
+        </div>
+      </focus-trap>
+    </template>
   </Transition>
 
   <ViewDridOnHover :defaultCount="count" :count="countOnHover" :isShow="isShowGrid" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import ViewDridOnHover from './ViewDridOnHover.vue';
 
-defineProps<{
+const props = defineProps<{
   isShowPopper: boolean;
   count: 3 | 5;
 }>();
@@ -39,6 +43,15 @@ defineEmits<{
   (e: 'newColors'): void;
   (e: 'clear'): void;
 }>();
+
+watch(
+  () => props.isShowPopper,
+  (newValue) => {
+    if (!newValue) {
+      onMouseleave();
+    }
+  }
+);
 
 const countOnHover = ref<3 | 5>(3);
 const isShowGrid = ref(false);
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import './assets/css/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { FocusTrap } from 'focus-trap-vue';
+
 import 'tippy.js/dist/tippy.css';
 
 import 'vue3-toastify/dist/index.css';
 
-createApp(App).mount('#root-app');
+createApp(App).component('FocusTrap', FocusTrap).mount('#root-app');
